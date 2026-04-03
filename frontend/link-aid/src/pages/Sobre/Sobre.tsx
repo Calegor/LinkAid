a5ec73e3823b406c9128b0db11d17c746f0451c4
julia :) <juliavaleriogs@gmail.com>
2026-04-03 20:06:48 -0300

    feat: add AboutTech component on Sobre page with project stack

--- frontend/link-aid/src/pages/Sobre/Sobre.tsx
+++ frontend/link-aid/src/pages/Sobre/Sobre.tsx
@@ -4,6 +4,7 @@ import AboutScrollCards from "../../components/AboutScrollCards/AboutScrollCards
 import AboutFeaturesOrbit from "../../components/AboutFeaturesOrbit/AboutFeaturesOrbit";
 import AboutPerformance from "../../components/AboutPerformance/AboutPerformance";
 import AboutProject from "../../components/AboutProject/AboutProject";
+import AboutTech from "../../components/AboutTech/AboutTech";
 
 const Sobre = () => {
   return (
@@ -13,6 +14,7 @@ const Sobre = () => {
       <AboutFeaturesOrbit />
       <AboutPerformance />
       <AboutProject />
+      <AboutTech />
     </main>
   );
 };
