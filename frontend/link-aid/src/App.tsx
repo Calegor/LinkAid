ecf48422c7433d3dae7f535acec26fac3a6bf7bb
julia :) <juliavaleriogs@gmail.com>
2026-04-03 22:45:29 -0300

    feat: implement ScrollToTop to reset scroll position on page change

--- frontend/link-aid/src/App.tsx
+++ frontend/link-aid/src/App.tsx
@@ -5,10 +5,13 @@ import Sobre from "./pages/Sobre/Sobre";
 import Equipe from "./pages/Equipe/Equipe";
 import Faq from "./pages/Faq/Faq";
 import Contato from "./pages/Contato/Contato";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Home />} />
