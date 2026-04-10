8152664224d1a62b00f5a8db5352975c9050f8e3
julia :) <juliavaleriogs@gmail.com>
2026-04-09 23:13:55 -0300

    feat: make contact links functional and personalize form success message

--- frontend/link-aid/src/components/ContactForm/ContactForm.tsx
+++ frontend/link-aid/src/components/ContactForm/ContactForm.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-//verificar se está no modelo que o professor pediu, porque só tivemos uma aula até agora sobre
 import {
   User,
   Mail,
@@ -26,6 +25,7 @@ type ContactFormData = z.infer<typeof contactSchema>;
 
 const ContactForm = () => {
   const [isSuccess, setIsSuccess] = useState(false);
+  const [submittedName, setSubmittedName] = useState("");
 
   const {
     register,
@@ -36,11 +36,13 @@ const ContactForm = () => {
     resolver: zodResolver(contactSchema),
   });
 
-  const onSubmit = async (data: ContactFormData) => {
-    console.log("LinkAid - Dados do formulário:", data);
+  const onSubmit: SubmitHandler<ContactFormData> = async (data) => {
+    setSubmittedName(data.name.split(" ")[0]);
+
     await new Promise((resolve) => setTimeout(resolve, 2000));
     setIsSuccess(true);
     reset();
+
     setTimeout(() => {
       setIsSuccess(false);
     }, 5000);
@@ -51,9 +53,7 @@ const ContactForm = () => {
       <div className="absolute top-0 left-[-10%] w-[300px] lg:w-[500px] h-[300px] lg:h-[500px] bg-blue-100/30 blur-[80px] lg:blur-[120px] rounded-full pointer-events-none -z-0" />
 
       <div className="container mx-auto px-6 max-w-[1200px] relative z-10 lg:pt-3">
-        {/* grid */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center lg:pl-32">
-          {/* text */}
           <div className="flex flex-col text-center lg:text-right order-1 lg:order-2 lg:pr-10">
             <div className="flex flex-col gap-4 mb-10 items-center lg:items-end">
               <h2 className="text-[12vw] sm:text-[9vw] md:text-[6vw] lg:text-[4.5vw] font-bold text-slate-950 tracking-tighter leading-[0.85] md:leading-[0.85] lg:leading-[0.8]">
@@ -70,48 +70,27 @@ const ContactForm = () => {
             </div>
           </div>
 
-          {/* from */}
           <div className="w-full flex justify-center lg:justify-start order-2 lg:order-1 mt-40">
             <div className="relative w-full max-w-[480px]">
-              {/* balao robo */}
-              <div
-                className="absolute 
-                -top-50 left-1/2 -translate-x-1/2 w-[240px]
-                md:-top-25 md:left-[65px] md:right-auto md:translate-x-0
-                z-30 animate-bounce duration-[2000ms]"
-              >
+              <div className="absolute -top-50 left-1/2 -translate-x-1/2 w-[240px] md:-top-25 md:left-[65px] md:right-auto md:translate-x-0 z-30 animate-bounce duration-[2000ms]">
                 <div className="bg-white border-[1.5px] border-slate-900/30 p-5 rounded-[2.5rem] shadow-[0_10px_30px_rgba(0,0,0,0.06)] relative">
                   <p className="font-mono text-[11px] font-medium text-slate-700 text-center italic leading-tight">
                     Bip-bop! Identifique-se para que eu possa encaminhar sua
                     mensagem ao setor correto.
                   </p>
-
-                  {/* seta balao */}
-                  <div
-                    className="absolute bg-white w-3 h-3 border-slate-900/30
-                    -bottom-1.5 left-1/2 -translate-x-1/2 rotate-45 border-r-[1.5px] border-b-[1.5px]
-                    md:top-auto md:bottom-8 md:-left-1.5 md:right-auto md:translate-x-0 
-                    md:rotate-[135deg] md:border-r-[1.5px] md:border-b-[1.5px] md:border-t-0 md:border-l-0"
-                  ></div>
+                  <div className="absolute bg-white w-3 h-3 border-slate-900/30 -bottom-1.5 left-1/2 -translate-x-1/2 rotate-45 border-r-[1.5px] border-b-[1.5px] md:top-auto md:bottom-8 md:-left-1.5 md:right-auto md:translate-x-0 md:rotate-[135deg] md:border-r-[1.5px] md:border-b-[1.5px]"></div>
                 </div>
               </div>
 
-              {/* robo */}
               <picture>
                 <source media="(max-width: 768px)" srcSet={robotMobile} />
                 <img
                   src={robotDesktop}
                   alt="Robot"
-                  className="absolute 
-                    -top-25 left-1/2 -translate-x-1/2 w-[150px] 
-                    md:-top-24 md:right-auto md:left-[-87px] md:translate-x-0 md:w-[150px] 
-                    lg:-top-29 lg:left-[-105px] lg:w-[180px] 
-                    scale-x-[-1]
-                    pointer-events-none select-none z-20 transition-all duration-500"
+                  className="absolute -top-25 left-1/2 -translate-x-1/2 w-[150px] md:-top-24 md:right-auto md:left-[-87px] md:translate-x-0 lg:-top-29 lg:left-[-105px] lg:w-[180px] scale-x-[-1] pointer-events-none select-none z-20 transition-all duration-500"
                 />
               </picture>
 
-              {/* form card */}
               <div
                 className="bg-white p-8 lg:p-10 rounded-[32px] border border-slate-100/60 relative z-10"
                 style={{
@@ -121,6 +100,7 @@ const ContactForm = () => {
               >
                 {!isSuccess ? (
                   <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                    {/* name */}
                     <div className="flex flex-col gap-2">
                       <label className="text-[10px] font-bold text-slate-900 uppercase tracking-[0.2em] ml-1">
                         Nome Completo
@@ -128,9 +108,9 @@ const ContactForm = () => {
                       <div
                         className={`flex items-center gap-4 px-5 py-3.5 rounded-2xl bg-white border transition-all duration-300 focus-within:shadow-md focus-within:border-blue-600 ${errors.name ? "border-red-200" : "border-slate-200"}`}
                       >
-                        <User size={18} className="text-slate-300" />
+                        <User size={18} className="text-slate-400" />
                         <input
-                          {...register("name")}
+                          {...register("name", { required: true })}
                           placeholder="Ex: João da Silva"
                           className="bg-transparent w-full outline-none text-slate-800 text-sm font-light placeholder:text-slate-400"
                         />
@@ -142,6 +122,7 @@ const ContactForm = () => {
                       )}
                     </div>
 
+                    {/* email */}
                     <div className="flex flex-col gap-2">
                       <label className="text-[10px] font-bold text-slate-900 uppercase tracking-[0.2em] ml-1">
                         E-mail Corporativo
@@ -149,9 +130,9 @@ const ContactForm = () => {
                       <div
                         className={`flex items-center gap-4 px-5 py-3.5 rounded-2xl bg-white border transition-all duration-300 focus-within:shadow-md focus-within:border-blue-600 ${errors.email ? "border-red-200" : "border-slate-200"}`}
                       >
-                        <Mail size={18} className="text-slate-300" />
+                        <Mail size={18} className="text-slate-400" />
                         <input
-                          {...register("email")}
+                          {...register("email", { required: true })}
                           placeholder="nome@empresa.com"
                           className="bg-transparent w-full outline-none text-slate-800 text-sm font-light placeholder:text-slate-400"
                         />
@@ -163,6 +144,7 @@ const ContactForm = () => {
                       )}
                     </div>
 
+                    {/* assunto */}
                     <div className="flex flex-col gap-2">
                       <label className="text-[10px] font-bold text-slate-900 uppercase tracking-[0.2em] ml-1">
                         Assunto
@@ -170,9 +152,9 @@ const ContactForm = () => {
                       <div
                         className={`flex items-center gap-4 px-5 py-3.5 rounded-2xl bg-white border transition-all duration-300 focus-within:shadow-md focus-within:border-blue-600 ${errors.subject ? "border-red-200" : "border-slate-200"}`}
                       >
-                        <MessageSquare size={18} className="text-slate-300" />
+                        <MessageSquare size={18} className="text-slate-400" />
                         <input
-                          {...register("subject")}
+                          {...register("subject", { required: true })}
                           placeholder="Ex: Parceria, Dúvidas..."
                           className="bg-transparent w-full outline-none text-slate-800 text-sm font-light placeholder:text-slate-400"
                         />
@@ -184,6 +166,7 @@ const ContactForm = () => {
                       )}
                     </div>
 
+                    {/* message */}
                     <div className="flex flex-col gap-2">
                       <label className="text-[10px] font-bold text-slate-900 uppercase tracking-[0.2em] ml-1">
                         Sua Mensagem
@@ -193,10 +176,10 @@ const ContactForm = () => {
                       >
                         <MessageSquare
                           size={18}
-                          className="text-slate-300 mt-1"
+                          className="text-slate-400 mt-1"
                         />
                         <textarea
-                          {...register("message")}
+                          {...register("message", { required: true })}
                           rows={2}
                           placeholder="Como podemos te ajudar?"
                           className="bg-transparent w-full outline-none text-slate-800 text-sm font-light placeholder:text-slate-400 resize-none"
@@ -233,9 +216,13 @@ const ContactForm = () => {
                     <h3 className="text-2xl font-bold text-slate-950 tracking-tight mb-2">
                       Mensagem recebida!
                     </h3>
-                    <p className="text-slate-500 font-light text-sm">
-                      Obrigado Julia, entraremos em contato.{" "}
-                      {/* IMPLEMENTAR O TYPESCRIPT AQUI PRA COLOCAR O NOME DA PESSOA QUANDO ELA ENVIAR O FORMULARIO */}
+                    <p className="text-slate-500 font-light text-sm max-w-[280px]">
+                      Obrigado,{" "}
+                      <span className="font-bold text-blue-600">
+                        {submittedName}
+                      </span>
+                      !<br />
+                      Nossa equipe entrará em contato em menos de 24h.
                     </p>
                   </div>
                 )}
@@ -247,4 +234,5 @@ const ContactForm = () => {
     </section>
   );
 };
+
 export default ContactForm;
